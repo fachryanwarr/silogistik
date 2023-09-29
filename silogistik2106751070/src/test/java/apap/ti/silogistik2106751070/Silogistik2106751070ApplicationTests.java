28d26eb7ce9ec2d1e26afe19b09617f1224fe4b9
Fachry Anwar <fachryanwar2926@gmail.com>
2023-09-29 23:06:11 +0700

    chore: initial project & .gitignore

---
+++ silogistik2106751070/src/test/java/apap/ti/silogistik2106751070/Silogistik2106751070ApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti.silogistik2106751070;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Silogistik2106751070ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
